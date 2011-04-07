88011c3ca4396b868b5454c3521b966d14aba2c9
Jeremy Jones <jeremyj@letifer.org>
2011-04-06 21:34:21 -0500

    testing thrift .60

---
+++ scribeif.c
@@ -0,0 +1,16 @@
+#include "scribe.h"
+#include <transport/thrift_socket.h>
+#include <transport/thrift_buffered_transport.h>
+#include <protocol/thrift_binary_protocol.h>
+
+int main(char **argc, int argv){
+    ThriftSocket *socket;
+    ThriftBufferedTransport *transport;
+    ThriftBinaryProtocol *protocol;
+
+    scribeClient *client;
+
+
+
+
+}
